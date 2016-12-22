44736413f0546be2e5df0e07eb7df454f7c2fd33
Laurent Wouters <lwouters@cenotelie.fr>
2016-12-22 21:01:25 +0100

    [fix] Implement the product description Mojo

--- xowl-product-builder-maven-plugin/src/main/java/org/xowl/toolkit/builder/BuilderMojo.java
+++ xowl-product-builder-maven-plugin/src/main/java/org/xowl/toolkit/builder/BuilderMojo.java
@@ -17,10 +17,19 @@
 
 package org.xowl.toolkit.builder;
 
+import org.apache.maven.model.Model;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.xowl.infra.utils.TextUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 
 /**
  * The Mojo for the xOWL product builder
@@ -29,8 +38,42 @@ import org.apache.maven.plugins.annotations.Mojo;
  */
 @Mojo(name = "package")
 public class BuilderMojo extends AbstractMojo {
+    /**
+     * The model for the current project
+     */
+    @Parameter(readonly = true, defaultValue = "${project.model}")
+    private Model model;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Hello, world.");
+        File targetDirectory = new File(model.getBuild().getDirectory());
+        File productFile = new File(targetDirectory, model.getArtifactId() + "-" + model.getVersion() + ".product");
+        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(productFile), Charset.forName("UTF-8"))) {
+            writer.write("{\n");
+            writer.write("\t\"type\": \"org.xowl.infra.utils.product.Product\",\n");
+            writer.write("\t\"identifier\": \"" + TextUtils.escapeStringJSON(model.getGroupId() + "." + model.getArtifactId()) + "\",\n");
+            writer.write("\t\"name\": \"" + TextUtils.escapeStringJSON(model.getName()) + "\",\n");
+            writer.write("\t\"description\": \"" + TextUtils.escapeStringJSON(model.getDescription()) + "\",\n");
+            writer.write("\t\"version\": {\n");
+            writer.write("\t\t\"number\": \"" + TextUtils.escapeStringJSON(model.getVersion()) + "\",\n");
+            writer.write("\t\t\"scmTag\": \"\",\n");
+            writer.write("\t\t\"buildUser\": \"\",\n");
+            writer.write("\t\t\"buildTag\": \"\",\n");
+            writer.write("\t\t\"buildTimestamp\": \"\"\n");
+            writer.write("\t},\n");
+            writer.write("\t\"copyright\": \"Copyright (c) " + TextUtils.escapeStringJSON(model.getOrganization().getName()) + "\",\n");
+            writer.write("\t\"vendor\": \"" + TextUtils.escapeStringJSON(model.getOrganization().getName()) + "\",\n");
+            writer.write("\t\"vendorLink\": \"" + TextUtils.escapeStringJSON(model.getOrganization().getUrl()) + "\",\n");
+            writer.write("\t\"link\": \"" + TextUtils.escapeStringJSON(model.getUrl()) + "\",\n");
+            writer.write("\t\"license\": {\n");
+            if (!model.getLicenses().isEmpty()) {
+                writer.write("\t\t\"name\": \"" + TextUtils.escapeStringJSON(model.getLicenses().get(0).getName()) + "\",\n");
+                writer.write("\t\t\"fullText\": \"" + TextUtils.escapeStringJSON(model.getLicenses().get(0).getUrl()) + "\"\n");
+            }
+            writer.write("\t}\n");
+            writer.write("}\n");
+        } catch (IOException exception) {
+            throw new MojoFailureException("Failed to write the product description", exception);
+        }
     }
 }
