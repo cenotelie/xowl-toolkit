9e4b3d24302333298689b6911e92de0c3da2d3e3
Laurent Wouters <lwouters@cenotelie.fr>
2017-02-07 10:26:29 +0100

    [clean] Some cleanup

--- xowl-packaging-maven-plugin/src/main/java/org/xowl/toolkit/packaging/Category.java
+++ xowl-packaging-maven-plugin/src/main/java/org/xowl/toolkit/packaging/Category.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Association Cénotélie (cenotelie.fr)
+ * Copyright (c) 2017 Association Cénotélie (cenotelie.fr)
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3
